c27c123b99c88661e1cadfb4477532e09250fc42
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-22 07:09:01 -0300

    Funcao soma

--- aula1.py
+++ aula1.py
@@ -1,9 +1,15 @@
 # Funcoes #
 
-def minhafuncao(parametro:
+def minhafuncao(parametro):
     print parametro
 
 ## Escreva uma funcao que some dois numeros
+
+def soma(num1, num2):
+    resposta = num1 + num2
+    return resposta
+
+print soma(2, 3)
 
 
 ## dica: python meuarquivo.py
