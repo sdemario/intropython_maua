d142f83af8ac31eee5e5ec37b6a692b775fd8469
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-22 09:07:12 -0300

    Listas

--- aula1.py
+++ aula1.py
@@ -1,1 +1,18 @@
-# Faca um programa que verifique se um numero e par
+### LISTAS ###
+
+a = [1, 2, 3]
+b = ["um", "dois", "tres", "quatro", "cinco"]
+
+list.append(x)
+list.extend(L)
+list.insert(i, x)
+list.remove(x)
+list.pop([i])
+list.index(x)
+list.count(x)
+list.sort(cmp=None, key=None, reverse=False)
+list.reverse()
+
+### Slice  ###
+
+print b[2:4]
