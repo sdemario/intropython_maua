1f530b6c1124b1005e559469ea49f662ffc3b8bb
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-22 07:03:05 -0300

    clausula if 2

--- aula1.py
+++ aula1.py
@@ -1,10 +1,19 @@
 # clausula IF ##
 
 a = 10
+b = 20
 
-if a > 10:
-    print "a Maior que 10"
-elif a < 10:
-    print "a Maior que 10"
-else
-    print "a Igual a 10"
+if a > 10 and b > 10:
+    print "a e b Maior que 10"
+
+if a > 10 or b > 10:
+    print "a ou b Maior que 10"
+
+if a >= 10:
+    print "a Maior  ou igual a 10"
+
+if a <= 10:
+    print "a Menor  ou igual a 10"
+
+if a == 10:
+    print "a  a 10"
