19202a73646687192712e98065be2687d2caf581
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-22 06:51:00 -0300

    Operacoes aritimeticas

--- aula1.py
+++ aula1.py
@@ -1,27 +1,11 @@
 
-### VARIAVEIS ###
+### OPERACOES ###
 
-#Numbers
-a = 2
+print (4 + 2)
+print (4 - 2)
+print (4 * 2)
+print (4 / 2)
+print (5 % 2)
+print (4 ** 2)
+print (5 // 2)
 
-#Strings
-b = "Hello World!"
-
-#Lists
-c = ['item 1', 'item 2', 3]
-
-#Dictionaries
-d = {'chave 1' : 'valor 1', 'chave 2': 2}
-
-#Tuples
-e = ('valor 1', 1)
-
-#Booleans
-f = True
-g = False
-
-#None
-h = None
-
-#Sets, types
-#Files
