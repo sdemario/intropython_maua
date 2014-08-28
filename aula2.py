1752485104fbaa4b8fdc697ab80fe35cdfe76086
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-28 16:47:19 -0300

    Aula 2: conteúdo da aula passada

---
+++ aula2.py
@@ -0,0 +1,16 @@
+'''
+
+AULA PASSADA
+-----------------------------
+
+ - Introdução ao Curso
+ - Intrudução ao Python
+ - Tipos
+ - Operações
+ - Principais Clausulas
+ - Variáveis
+ - Função
+ - Listas
+ - Loops
+ - Introdução a módulos
+'''
