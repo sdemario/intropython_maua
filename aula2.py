638b87aa6460b873488338460e13cb69e4b00984
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-28 17:35:27 -0300

    Medias x Notas: resposta

--- aula2.py
+++ aula2.py
@@ -7,3 +7,15 @@ Faca um programa que leia notas de diversos alunos e calcule a media
 a lista termina quando e digitado um numero negativo
 
 '''
+
+notas = []
+n = int(raw_input("digite a nota: "))
+while n >= 0:
+    notas.append(n)
+    n = int(raw_input("digite a nota: "))
+
+soma = 0
+for i in notas:
+    soma = soma + i
+
+print soma / len(notas)
