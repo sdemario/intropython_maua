a9e37a6cf75be279907e506f382e02c0a463d9ff
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-28 19:03:07 -0300

    Classe estatica Pessoa

--- aula2.py
+++ aula2.py
@@ -3,29 +3,20 @@
 CLASSES - INTRODUCAO
 ----------------------------
 
-- Programacao Orientada a Objetos
-  A orientação a objetos é um paradigma de análise, projeto e programação de
-  sistemas de software baseado na composição e interação entre diversas
-  unidades de software chamadas de objetos. [wikipedia]
-
 - Classe
-  Em orientação a objetos, uma classe é uma estrutura que abstrai um conjunto de
-  objetos com características similares. [wikipedia]
+  Em orientacao a objetos, uma classe e uma estrutura que abstrai um conjunto de
+  objetos com caracteristicas similares. [wikipedia]
 
   - Estrutura da classe
        - Construtores
        - Destrutor
        - Propriedades/Atributos
        - Eventos/Metodos
-
-  - Encapsulamento
-  - Herança
-  - Polimorfismo
-  - Associação
-    - Agregação
-    - Composição
-  - Classes abstratas e concretas
-
 '''
 
+class Pessoa:
+    nome = "Nome da Pessoa"
+    idade = 100
 
+print Pessoa.nome
+print Pessoa.idade
