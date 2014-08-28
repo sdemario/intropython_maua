a87bdc7a963b7c17bec37d75f1b0de0780f3618c
Sheldon Demario <sheldon@telefonicabeta.com>
2014-08-28 19:40:00 -0300

    Classes: metodo

--- aula2.py
+++ aula2.py
@@ -17,12 +17,19 @@ CLASSES - CLASSE x INSTANCIA
 class Pessoa:
     nome = "Nome da Pessoa"
     idade = 100
+    profissao = None
 
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
 
+    def set_profissao(self, profissao):
+        self.profissao = profissao
+
 p = Pessoa("Sheldon", 29)
 
 print p.nome
 print p.idade
+p.set_profissao("Programador")
+
+print p.profissao
