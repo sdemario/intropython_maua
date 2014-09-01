c0a258edfa64938d2e83fe660fd3954b5e8b768a
Sheldon Demario <sheldon@telefonicabeta.com>
2014-09-01 10:53:42 -0300

    Aula 3: conteudo da aula passada

---
+++ aula3.py
@@ -0,0 +1,21 @@
+'''
+
+AULA PASSADA
+------------------------
+
+- Python Interpreter x Scripts
+- Excecoes e Erros
+- Introducao a OO
+- Definicao de Classes
+- Metodos
+- Heranca
+- Reimplementacao de funcao media para metodo de classe
+
+- Classes x Modulos
+- Associacao
+- Exercicio
+  - Descricao de notas
+  - Metodo idade por data de nascimento
+
+
+'''
