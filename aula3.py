e85c8933fbbc4d2d143470e8e8e97796e9769aad
Sheldon Demario <sheldon@telefonicabeta.com>
2014-09-04 21:52:34 -0300

    Aula 3: conteudo

--- aula3.py
+++ aula3.py
@@ -1,21 +1,11 @@
 '''
 
-AULA PASSADA
+BANCO DE DADOS e SQL
 ------------------------
 
-- Python Interpreter x Scripts
-- Excecoes e Erros
-- Introducao a OO
-- Definicao de Classes
-- Metodos
-- Heranca
-- Reimplementacao de funcao media para metodo de classe
-
-- Classes x Modulos
-- Associacao
-- Exercicio
-  - Descricao de notas
-  - Metodo idade por data de nascimento
-
+ - persistir dados
+ - relacionamentos
+ - abstracao
+ - Projeto: estruturacao Classes
 
 '''
