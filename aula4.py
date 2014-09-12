245b139b95f1daa51bbf78df698bfdd761df5dc3
Sheldon Demario <sheldon@telefonicabeta.com>
2014-09-12 11:27:15 -0300

    Exercicios revisao

---
+++ aula4.py
@@ -0,0 +1,44 @@
+REVISAO
+=====================
+
+- Tipos
+- Funcoes
+- Excecoes
+- Listas
+- Strings
+- Loops
+- Iterators
+- Python console x Python scripts
+- Classes e OO
+- Modulos
+- Banco de dados
+
+1 - Faca um programa que leia 3 notas e calcule a media delas
+
+2 - Faca um progrma que verifique se um numero e par/impar
+
+3 - Faca um programa que leia as notas de diversos alunos e calcule a nota media
+    A lista termina quando e digitado um numero negativo
+
+4 - Faca um programa que leia uma string e mostre ela ao contrario
+
+5 - Faca um programa que leia o nome e a nota de diversos alunos.
+  - Condicao de parada: quando o nome for vazio
+
+6 - Baseada na lista anterior, escreva um programa que mostre a lista de alunos,
+sua nota e um numero que representa a quantidade de palavras no nome do aluno.
+Ex.: Se o aluno se chama "Sheldon Demario", o numero de palavras retornado eh 2.
+Se o aluno se chama "Joao da Silva Alves", o numero de plavras retornado sera 4.
+
+7 - Faca um programa que leia diversas notas de um aluno. Deve haver um metodo
+para mostrar a media do aluno, a maior nota e a menor nota
+
+8 - Baseada na lista anterior, faca um programa que mostre a lista de notas de
+um aluno, com sua respectiva descricao. A exemplo:
+"Aluno 1"
+  - "Trabalho 1", 9.5
+  - "Exercicio", 8.7
+  - "Prova final", 7.5
+
+9 - Faca um programa que mostre caracteristicas comund de animais e um metodo
+"falar" em que o programa imprime o som que o animal faz
